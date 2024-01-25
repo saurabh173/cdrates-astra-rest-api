5efe62cdaecd9abfbb71ef0f8d246296a7e3666b
Numero Uno <numerounodeveloper@gmail.com>
2024-01-24 22:47:36 -0500

    Fixed GraphQL filtering

--- src/main/java/com/example/demo/controller/GraphQLController.java
+++ src/main/java/com/example/demo/controller/GraphQLController.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.example.demo.dao.BankCDRatesDao;
 import com.example.demo.model.CDRates;
 import com.example.demo.model.CDRatesWithoutManagerRate;
 import com.example.demo.service.RateUtility;
@@ -10,15 +11,13 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.core.env.Environment;
 import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Controller
 public class GraphQLController {
@@ -30,6 +29,9 @@ public class GraphQLController {
 
     @Autowired
     RestTemplate restTemplate;
+
+    @Autowired
+    BankCDRatesDao dao;
 
     @Autowired
     private ObjectMapper objectMapper;
@@ -48,66 +50,27 @@ public class GraphQLController {
         HttpHeaders headers = new HttpHeaders();
         headers.set("x-cassandra-token", env.getProperty("ASTRA_DB_APPLICATION_TOKEN"));
         HttpEntity<String> httpEntity = new HttpEntity<String>(headers);
+        List<CDRates> customList =
+                dao.getCDRates().stream()
+                        .filter(rate -> state.equals(rate.getStatecode()))
+                        .collect(Collectors.toList());
 
-
-        //String whereCondition = "{\"statecode\" : {\"$eq\" : \"" + state + "\"}}";
-
-        //JSONParser parser = new JSONParser();
-        //JSONObject json = (JSONObject) parser.parse(whereCondition);
-
-        String whereClause = "{\"statecode\": {\"$eq\": \"" + state + "\"}}";
-
-        ResponseEntity<Map<String, Object>> responseEntity = restTemplate.exchange(
-                "https://" +
-                        env.getProperty("ASTRA_DB_ID") +
-                        "-" +
-                        env.getProperty("ASTRA_DB_REGION") +
-                        ".apps.astra.datastax.com/api/rest/v2/keyspaces/" +
-                        env.getProperty("ASTRA_DB_KEYSPACE") +
-                        "/cdrates?where={}" ,
-                HttpMethod.GET,
-                httpEntity,
-                new ParameterizedTypeReference<Map<String, Object>>() {}
-        );
-
-        List<CDRates> customList = objectMapper.convertValue(responseEntity.getBody().get("data"), new TypeReference<List<CDRates>>() {});
         return customList;
+
     }
 
     @QueryMapping
     public List<CDRatesWithoutManagerRate> ratesByZip(@Argument String zip) {
         String state = conversionUtility.getState(zip);
-        if (state.equals("")){
+        if (state.equals("")) {
             return null;
         }
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("x-cassandra-token", env.getProperty("ASTRA_DB_APPLICATION_TOKEN"));
-        HttpEntity<String> httpEntity = new HttpEntity<String>(headers);
+        List<CDRatesWithoutManagerRate> customList =
+                dao.getCDRatesWithoutManagerRate().stream()
+                        .filter(rate -> state.equals(rate.getStatecode()))
+                        .collect(Collectors.toList());
 
-
-        //String whereCondition = "{\"statecode\" : {\"$eq\" : \"" + state + "\"}}";
-
-        //JSONParser parser = new JSONParser();
-        //JSONObject json = (JSONObject) parser.parse(whereCondition);
-
-        String whereClause = "{\"statecode\": {\"$eq\": \"" + state + "\"}}";
-
-        ResponseEntity<Map<String, Object>> responseEntity = restTemplate.exchange(
-                "https://" +
-                        env.getProperty("ASTRA_DB_ID") +
-                        "-" +
-                        env.getProperty("ASTRA_DB_REGION") +
-                        ".apps.astra.datastax.com/api/rest/v2/keyspaces/" +
-                        env.getProperty("ASTRA_DB_KEYSPACE") +
-                        "/cdrates?where={}" ,
-                HttpMethod.GET,
-                httpEntity,
-                new ParameterizedTypeReference<Map<String, Object>>() {}
-        );
-
-        List<CDRatesWithoutManagerRate> customList = objectMapper.convertValue(responseEntity.getBody().get("data"), new TypeReference<List<CDRatesWithoutManagerRate>>() {});
         return customList;
     }
-
 }
